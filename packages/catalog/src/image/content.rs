eacba56bd0436044115b5913fa66877f32803227
Simon Janssen <janssen.simon@posteo.de>
2025-05-11 17:16:41 +0200

    add barcode reader node

--- packages/catalog/src/image/content.rs
+++ packages/catalog/src/image/content.rs
@@ -1,3 +1,19 @@
+
+use flow_like::flow::node::NodeLogic;
+use std::sync::Arc;
+
+pub mod read_barcodes;
 pub mod read_from_path;
 pub mod read_from_url;
 pub mod write_to_path;
+
+/// Content-Related Image Nodes
+pub async fn register_functions() -> Vec<Arc<dyn NodeLogic>> {
+    let nodes: Vec<Arc<dyn NodeLogic>> = vec![
+        Arc::new(read_barcodes::ReadBarcodesNode::default()),
+        Arc::new(read_from_path::ReadImagePathNode::default()),
+        Arc::new(read_from_url::ReadImageFromUrlNode::default()),
+        Arc::new(write_to_path::WriteImageNode::default()),
+    ];
+    nodes
+}
---
+++ packages/catalog/src/image/content/read_barcodes.rs
@@ -0,0 +1,204 @@
+
+use crate::image::NodeImage;
+use flow_like::{
+    flow::{
+        board::Board,
+        execution::context::ExecutionContext,
+        node::{Node, NodeLogic},
+        pin::PinOptions,
+        variable::VariableType,
+    },
+    state::FlowLikeState,
+};
+use flow_like_types::{
+    anyhow, async_trait, json::json,
+    rxing::{helpers, BarcodeFormat, RXingResult, DecodeHints},
+    Ok, Error
+};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashSet, sync::Arc};
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct BarcodePoint {
+    pub x: f32,
+    pub y: f32
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct Barcode {
+    text: String,
+    raw_bytes: Vec<u8>,
+    num_bits: usize,
+    format: String,
+    timestamp: u128,
+    line_count: usize,
+    points: Vec<BarcodePoint>,
+}
+
+impl From<RXingResult> for Barcode {
+    fn from(value: RXingResult) -> Self {
+        let points = value.getPoints()
+            .into_iter()
+            .map(|p| BarcodePoint { x: p.x, y: p.y } )
+            .collect();
+        Barcode { 
+            text: value.getText().to_string(), 
+            raw_bytes: value.getRawBytes().to_vec(), 
+            num_bits: value.getNumBits(), 
+            format: value.getBarcodeFormat().to_string(), 
+            timestamp: value.getTimestamp(), 
+            line_count: value.line_count(), 
+            points,
+        }
+    }
+}
+
+/// # Detect and Decode (Bar)codes in Images
+#[derive(Default)]
+pub struct ReadBarcodesNode {}
+
+impl ReadBarcodesNode {
+    pub fn new() -> Self {
+        ReadBarcodesNode {}
+    }
+}
+
+#[async_trait]
+impl NodeLogic for ReadBarcodesNode {
+    async fn get_node(&self, _app_state: &FlowLikeState) -> Node {
+        let mut node = Node::new(
+            "read_barcodes",
+            "Read Barcodes",
+            "Read/Decode Barcodes",
+            "Image/Content",
+        );
+        node.add_icon("/flow/icons/image.svg");
+
+        // inputs
+        node.add_input_pin("exec_in", "Input", "Initiate Execution", VariableType::Execution);
+
+        node.add_input_pin("image_in", "Image", "Image object", VariableType::Struct)
+            .set_schema::<NodeImage>()
+            .set_options(PinOptions::new().set_enforce_schema(true).build());
+
+        
+        node.add_input_pin("multiple", "Multiple?", "Detect Multiple Codes in Image", VariableType::Boolean)
+            .set_default_value(Some(json!(true)));
+
+        node.add_input_pin("filter", "Filter?", "Filter for Certain Code Type", VariableType::Boolean)
+            .set_default_value(Some(json!(false)));
+
+        // outputs
+        node.add_output_pin("exec_out", "Output", "Done with the Execution", VariableType::Execution);
+
+        node.add_output_pin("results", "Results", "Detected/Decoded Codes", VariableType::Struct)
+            .set_schema::<Barcode>()
+            .set_value_type(flow_like::flow::pin::ValueType::Array);
+
+        node
+    }
+
+    async fn run(&self, context: &mut ExecutionContext) -> flow_like_types::Result<()> {
+        context.deactivate_exec_pin("exec_out").await?;
+
+        // fetch inputs
+        let multiple: bool = context.evaluate_pin("multiple").await?;
+        let apply_filter: bool = context.evaluate_pin("filter").await?;
+        let node_img: NodeImage = context.evaluate_pin("image_in").await?;
+
+        // prepare image
+        let img = node_img.get_image(context).await?;
+        let img_guard = img.lock().await;
+        let (w, h) = (img_guard.width(), img_guard.height());
+        let img_vec = img_guard
+            .clone()
+            .into_luma8()  // decoding works best with grayscale images
+            .to_vec();
+
+        // detect + decode (bar)codes
+        let results_rxing = match (multiple, apply_filter) {
+            // many codes & many types (potentially expensive)
+            (true, false) => {
+                helpers::detect_multiple_in_luma(img_vec, w, h)?
+            },
+            // many codes & single type
+            (true, true) => {
+                let mut hints = DecodeHints::default();
+                let format_str: String = context.evaluate_pin("format").await?;
+                let bc_type = BarcodeFormat::from(format_str);
+                hints.PossibleFormats = Some(HashSet::from([bc_type]));
+                helpers::detect_multiple_in_luma_with_hints(img_vec, w, h, &mut hints)?
+            },
+            // one code & one type
+            (false, true) => {
+                let format_str: String = context.evaluate_pin("format").await?;
+                let bc_type = BarcodeFormat::from(format_str);
+                vec![helpers::detect_in_luma_filtered(img_vec, w, h, Some(bc_type))?]
+            },
+            // one code & many types
+            (false, false) => {
+                vec![helpers::detect_in_luma(img_vec, w, h, None)?]
+            },
+        };
+
+        // map to serializable results
+        let results = results_rxing
+            .into_iter()
+            .map(Barcode::from)
+            .collect::<Vec<Barcode>>();
+
+        // set outputs
+        context.set_pin_value("results", json!(results)).await?;
+        context.activate_exec_pin("exec_out").await?;
+        Ok(())
+    }
+
+    async fn on_update(&self, node: &mut Node, _board: Arc<Board>) {
+        let apply_filter = node
+            .get_pin_by_name("filter")
+            .and_then(|pin| pin.default_value.clone())
+            .and_then(|bytes| flow_like_types::json::from_slice::<bool>(&bytes).ok())
+            .unwrap_or_default();
+
+        let format_pin = node.get_pin_by_name("format");
+
+        if apply_filter {
+            if format_pin.is_some() {
+                return;
+            }
+            node.add_input_pin("format", "Format", "Barcode Format", VariableType::String)
+                .set_options(
+                    PinOptions::new()
+                        .set_valid_values(vec![
+                            "AZTEC".to_string(),
+                            "CODABAR".to_string(),
+                            "CODE_39".to_string(),
+                            "CODE_93".to_string(),
+                            "CODE_128".to_string(),
+                            "DATA_MATRIX".to_string(),
+                            "EAN_8".to_string(),
+                            "EAN_13".to_string(),
+                            "ITF".to_string(),
+                            "MAXICODE".to_string(),
+                            "PDF_417".to_string(),
+                            "QR_CODE".to_string(),
+                            "MICRO_QR_CODE".to_string(),
+                            "RECTANGULAR_MICRO_QR_CODE".to_string(),
+                            "RSS_14".to_string(),
+                            "RSS_EXPANDED".to_string(),
+                            "TELEPEN".to_string(),
+                            "UPC_A".to_string(),
+                            "UPC_E".to_string(),
+                            "UPC_EAN_EXTENSION".to_string(),
+                            "DXFilmEdge".to_string(),
+                        ])
+                        .build(),
+                );
+        } else {
+            if format_pin.is_some() {
+                node.pins.remove(&format_pin.id);
+            }
+        }
+    }
+}
