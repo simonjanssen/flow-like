d6af41b2b4b65237dd95f2c5975101cdf2ce5b9c
Simon Janssen <janssen.simon@posteo.de>
2025-07-06 15:43:22 +0200

    rename + enable openai definitions

---
+++ packages/catalog/src/utils/json/parse_with_schema.rs
@@ -0,0 +1,171 @@
+use std::fmt::format;
+
+use flow_like::{
+    flow::{
+        execution::{LogLevel, context::ExecutionContext},
+        node::{Node, NodeLogic},
+        variable::VariableType,
+    },
+    state::FlowLikeState,
+    utils::json::parse_malformed_json,
+};
+use flow_like_types::{Error, Value, anyhow, async_trait, json, jsonschema};
+
+/// Is this JSON an OpenAI Function Definition?
+/// https://platform.openai.com/docs/guides/function-calling?api-mode=responses#defining-functions
+pub fn is_openai(data: &Value) -> bool {
+    let map = match data.as_object() {
+        Some(m) => m,
+        None => return false,
+    };
+
+    // check "type" is "function"
+    if map.get("type") != Some(&Value::String("function".into())) {
+        return false;
+    }
+
+    // check required fields
+    for key in ["name", "description", "parameters", "strict"] {
+        if map.get(key).map_or(true, Value::is_null) {
+            return false;
+        }
+    }
+    true
+}
+
+/// Converts OpenAI Function Defintion to JSON Schema
+/// Returns data as-is if not OpenAI
+pub fn into_json_schema(data: Value) -> Result<Value, Error> {
+    if is_openai(&data) {
+        if let Some(obj) = data.as_object() {
+            if let Some(parameters) = obj.get("parameters") {
+                if let Some(mut parameters_obj) = parameters.as_object().cloned() {
+                    parameters_obj.remove("additionalProperties");
+                    let name = match obj.get("name").cloned() {
+                        Some(name) => name,
+                        None => Value::Null,
+                    };
+                    parameters_obj.insert("title".to_string(), name);
+                    return Ok(Value::Object(parameters_obj));
+                }
+            }
+        }
+        return Err(anyhow!("Failed to convert OpenAI function to JSON Schema"));
+    } else {
+        Ok(data)
+    }
+}
+
+/// Validates JSON data against JSON/OpenAI Schema and returns JSON data as Value
+fn validate_json(definition_str: &str, data_str: &str) -> Result<Value, Error> {
+    // Is definition input pin value valid JSON?
+    let definition: Value = match json::from_str(&definition_str) {
+        Ok(definition) => definition,
+        Err(e) => {
+            return Err(anyhow!(format!(
+                "Failed to load definition/schema from input string: {}",
+                e
+            )));
+        }
+    };
+
+    // Convert defintion into JSON Schema spec
+    let schema = into_json_schema(definition)?;
+
+    // Create Schema Validator
+    let validator = match jsonschema::validator_for(&schema) {
+        Ok(validator) => validator,
+        Err(e) => return Err(anyhow!(format!("Failed to load schema validator: {}", e))),
+    };
+
+    // Is data input pin value valid JSON?
+    let data: Value = match json::from_str(data_str) {
+        Ok(data) => data,
+        Err(e) => return Err(anyhow!(format!("Failed to parse JSON data: {}", e))),
+    };
+
+    // Validate input data againts JSON schema
+    let errors = validator.iter_errors(&data);
+    let error_msg = errors
+        .map(|e| format!("Error: {}, Location: {}", e, e.instance_path))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    if error_msg.len() < 1 {
+        return Ok(data);
+    } else {
+        return Err(anyhow!(format!("Schema validation failed: {}", error_msg)));
+    }
+}
+
+#[derive(Default)]
+pub struct ParseWithSchema {}
+
+impl ParseWithSchema {
+    pub fn new() -> Self {
+        ParseWithSchema {}
+    }
+}
+
+#[async_trait]
+impl NodeLogic for ParseWithSchema {
+    async fn get_node(&self, _app_state: &FlowLikeState) -> Node {
+        let mut node = Node::new(
+            "parse_with_schema",
+            "Parse JSON with Schema",
+            "Parse JSON input Data With JSON/OpenAI Schema and Return Value",
+            "Utils/JSON",
+        );
+
+        node.add_icon("/flow/icons/repair.svg");
+
+        node.add_input_pin(
+            "exec_in",
+            "Input",
+            "Initiate Execution",
+            VariableType::Execution,
+        );
+
+        node.add_input_pin(
+            "definition",
+            "Definition",
+            "JSON/OpenAI Schema Definition",
+            VariableType::String,
+        );
+
+        node.add_input_pin(
+            "data",
+            "Data",
+            "JSON Input Data to be parsed",
+            VariableType::String,
+        );
+
+        node.add_output_pin(
+            "exec_out",
+            "Output",
+            "Execution continues if parsing succeeds",
+            VariableType::Execution,
+        );
+
+        node.add_output_pin(
+            "parsed",
+            "Parsed",
+            "Parsed and Validated JSON",
+            VariableType::Struct,
+        );
+
+        return node;
+    }
+
+    async fn run(&self, context: &mut ExecutionContext) -> flow_like_types::Result<()> {
+        context.deactivate_exec_pin("exec_out").await?;
+        let definition_str: String = context.evaluate_pin("definition").await?;
+        let data_str: String = context.evaluate_pin("data").await?;
+
+        let validated = validate_json(&definition_str, &data_str)?;
+
+        context.set_pin_value("parsed", validated).await?;
+        context.activate_exec_pin("exec_out").await?;
+        Ok(())
+    }
+}
